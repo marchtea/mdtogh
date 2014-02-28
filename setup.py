25aa848a5edbf5f7e564c1745dcc9b8951a56e5c
marchtea <marchtea213@gmail.com>
2014-02-28 08:50:49 +0800

    add files setuptools requires, still need test

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == 'publish':
+    sys.exit(os.system('python setup.py sdist upload'))
+
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='mdtogh',
+    version='1.0.0',
+    packages=find_packages(),
+    #install requirements
+    install_requires=read('requirements.txt'),
+
+    package_data={'mdtogh': ['templates/*']},
+
+    entry_points={'console_scripts': ['mdtogh = mdtogh.command:main']},
+
+    zip_safe=False,
+
+    author='Summer Ruan',
+    author_email='marchtea213@gmail.com',
+    description='Transform markdown files into html with styles of github',
+    long_description=read('descriptions.txt'),
+    url='http://github.com/marchtea/mdtogh',
+    platforms='any',
+)
